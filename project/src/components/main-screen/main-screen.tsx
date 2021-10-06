e357c440dfd752cc89c3e28ef4ff2a180a730496
trueotmor <proskurin.sasha78@gmail.com>
2021-10-06 17:23:54 +0300

    decomposed into components

---
+++ project/src/components/main-screen/main-screen.tsx
@@ -0,0 +1,56 @@
+import HeaderComponent from '../header-component/header-component';
+import MainTabsComponent from './main-tabs-component';
+import PlaceCardComponent from './place-card-component';
+
+
+type MainScreenProps = {
+  placesCount : number;
+}
+
+function MainScreen({placesCount}: MainScreenProps): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <HeaderComponent/>
+
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <MainTabsComponent/>
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{placesCount} places to stay in Amsterdam</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex={0}>
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
+                </ul>
+              </form>
+              <div className="cities__places-list places__list tabs__content">
+                <PlaceCardComponent/>
+                <PlaceCardComponent/>
+                <PlaceCardComponent/>
+                <PlaceCardComponent/>
+                <PlaceCardComponent/>
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map"></section>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default MainScreen;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACES_COUNT : NaN,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount = {Setting.PLACES_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
